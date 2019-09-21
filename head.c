3eedc08f29f4cf31be6405c38ae161ae76aaf010
dawum nam <dnam2@alaska.edu>
2019-09-20 23:08:44 -0800

    head finished

--- head.c
+++ head.c
@@ -1,25 +1,91 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <string.h>
+#include <stdlib.h>
+#include <errno.h>
 
-int my_file_puts(int fd,const char *s);
 int countChars(const char *s);
+int my_file_puts(int fd,const char *s);
 
-char buf;  
-
+int atooi(const char *s);
+char buf[1];  
+int lineMax, lineNo, fd;
 int main(int argc, char **argv)
 {
-    int lineNo = 0, lineMax = 10;
-    while (read(0,&buf,1)==1 && lineNo < lineMax)
-    {        
-        my_file_puts(1,&buf);
+    lineNo = 0;
+    if (argc == 1)
+    {
+        lineMax = 10;
+        fd = 0;
+    }
+    else if (argc == 2)
+    {
+        lineMax = 10;
+        fd = open(argv[1],O_RDONLY);
+        if (fd < 0) {
+           write(fileno(stderr),strerror(errno),countChars(strerror(errno)));
+           return 1;
+        }
+
+    }
+    else if (argc == 3)
+    {
+         if (argv[1][1] != 'n' || argv[1][0] != '-' || argv[1][2] != '\0')
+            {
+                write(fileno(stderr),"did you mean -n?",20);
+                return 1;
+            }
+            lineMax = atooi(argv[2]);
+            fd = 0;
+    }
+    else if (argc == 4)
+    {
+        if (argv[1][0] == '-' && argv[1][1] == 'n' && argv[1][2] == '\0')
+        {
+            lineMax = atooi(argv[2]);
+            fd = open(argv[3],O_RDONLY);
+            if (fd < 0) 
+            {
+                write(fileno(stderr),strerror(errno),countChars(strerror(errno)));
+                return 1;
+            }
+        }
+        else
+        {
+            if (argv[2][1] != 'n' || argv[2][0] != '-' || argv[2][2] != '\0')
+            {
+                write(fileno(stderr),"did you mean -n?",20);
+                return 1;
+            }
+            lineMax = atooi(argv[3]);
+            fd = open(argv[1],O_RDONLY);
+            if (fd < 0) 
+            {
+                write(fileno(stderr),strerror(errno),countChars(strerror(errno)));
+                return 1;
+            }
+
+        }
         
-        if (buf == '\n')
+    }
+    else
+    {
+        write(fileno(stderr),"did you mean -n?",20);
+        return 1;
+
+    }
+    while (read(fd,&buf,1)==1 && lineNo < lineMax)
+    {        
+        my_file_puts(1,buf);
+        
+        if (buf[0] == '\n')
         {
             lineNo++;
         }
         
     }
+    close(fd);
     return 0;
 }
 
@@ -35,10 +101,20 @@ int countChars(const char *s)
 int my_file_puts(int fd, const char *s)
 {
     int inputNumber = countChars(s);
-    if (fd==1)
-    {
-        write(fd, s, inputNumber);
-    }
+    write(fd, s, inputNumber);
     return inputNumber;
 }
+int atooi(const char *s)
+{
+    int offset = 0 , n = 0;
+    if (s[0] == '-') return n;
+    for (int i = offset; s[i] != '\0'; i++) 
+    {
+        n = n * 10 + s[i] - '0';
+    }   
+
+    return n;
+}
+
+
 
