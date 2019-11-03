a1bb7c2514fbf857a08105a5fe30a88228cbcf21
tacarpenter <42849099+tacarpenter@users.noreply.github.com>
2019-11-03 14:28:58 -0900

    Update implementation.c

--- implementation.c
+++ implementation.c
@@ -1,3 +1,4 @@
+
 /*  
   good example to look at
   https://embeddedartistry.com/blog/2017/2/9/implementing-malloc-first-fit-free-list#prerequisites=
@@ -148,16 +149,15 @@ typedef struct ll{
 #define SL sizeof(ll) //Size of LL
 #define BYTE sizeof(char) // 1 byte
 
-void __free_impl(void *ptr);
-
 // initialize LL to hold free memory
 ll * head = NULL;
+
+void __free_impl(void *ptr);
 
 // search the global LL for a large enough chunk of memory. If found, return that space and add 
 // leftover to the global LL if that leftover is large enough to hold the header.
 static void * searchLL(size_t size){
-    void * temp1 = head;
-//    ll * prev = NULL;
+    ll * temp1 = head;
 
     // check if head is NULL (LL is empty)
     if (temp1 == NULL)
@@ -175,6 +175,7 @@ static void * searchLL(size_t size){
 		if (GET_NEXT(temp1) == NULL && GET_PREV(temp1) == NULL) // temp1 is only member of LL
 		{
 		    head = NULL;
+		    return temp1;
 		}
 		else if (GET_PREV(temp1) == NULL) //temp1 is head of LL in this case
 		{
@@ -186,6 +187,7 @@ static void * searchLL(size_t size){
 		else if(GET_NEXT(temp1) == NULL) //temp1 is at tail
 		{
 		    GET_NEXT(GET_PREV(temp1)) = NULL;
+		    return temp1;
 		}
 		else //temp1 is somewhere in middle of LL in this case
 		{
@@ -198,16 +200,21 @@ static void * searchLL(size_t size){
 	    // Then, remove temp1 from LL, resize temp1, return temp1
 	    else
 	    {
-		// create new chunk of leftover to be inserted back into LL
-		void * temp2 = temp1 + (GET_LENGTH(temp1) - size - SL); // ***place pointer to start of new block created at proper place
-		GET_LENGTH(temp2) = (size + SL); // make temp2's length = exactly whats requested
+		// create new chunk of leftover to be inserted back into LL, resize temp1
+		ll * temp2;
+		char * temp3;
+		temp3 = (char *)temp1 + SL + size + 1; //place pointer for temp2 at end of space that will be used from temp1
+		temp2 = (ll *)temp3;
+		GET_LENGTH(temp2) = GET_LENGTH(temp1) - SL - size; // make temp1's length = exactly whats requested
 		GET_MMPSTART(temp2) = GET_MMPSTART(temp1);
 		GET_MMPSIZE(temp2) = GET_MMPSIZE(temp1);
+		GET_LENGTH(temp1) = SL + size;
 		// Check if at tail or head
-		if (GET_NEXT(temp1) == NULL && GET_PREV(temp1) == NULL) // temp1 is the only value in LL
+		if (GET_NEXT(temp1) == NULL && GET_PREV(temp1) == NULL) // temp1 is the only value in LL (and head)
 		{
 		    GET_NEXT(temp2) = NULL;
 		    GET_PREV(temp2) = NULL;
+		    head = temp2;
 		}
 		else if(GET_NEXT(temp1) == NULL) // temp1 is tail
 		{
@@ -220,6 +227,7 @@ static void * searchLL(size_t size){
 		    GET_PREV(temp2) = NULL;
 		    GET_NEXT(temp2) = GET_NEXT(temp1);
 		    GET_PREV(GET_NEXT(temp1)) = temp2;
+		    head = temp2;
 
 		}
 		else // temp1 is somewhere in middle of LL
@@ -229,8 +237,7 @@ static void * searchLL(size_t size){
 		    GET_NEXT(GET_PREV(temp1)) = temp2;
 		    GET_PREV(GET_NEXT(temp1)) = temp2;
 		}
-		// resize temp1
-		GET_LENGTH(temp1) = (GET_LENGTH(temp1) - (size + SL));
+		// Null out temp1's prev and next values
 		GET_NEXT(temp1) = NULL;
 		GET_PREV(temp1) = NULL;
 		return temp1;
@@ -250,82 +257,110 @@ static void tryMunmap(ll *ptr)
     {
 	if (GET_LENGTH(ptr) == GET_MMPSIZE(ptr))
 	{
-
-	    // ADD CHECK FOR NULL CASES ///
-
-
-	    // Remove it from free memory LL and munmap it
-	    GET_NEXT(GET_PREV(ptr)) = GET_NEXT(ptr);
-	    munmap(ptr,GET_MMPSIZE(ptr));
-	    return;
+	    if (GET_NEXT(ptr) == NULL && GET_PREV(ptr) == NULL) // ptr is only member of LL
+	    {
+         	head = NULL;
+	        munmap(ptr, GET_MMPSIZE(ptr));
+		return;
+	    }
+	    else if (GET_PREV(ptr) == NULL) // ptr is head of LL in this case
+	    {
+      	        head = GET_NEXT(ptr);
+		GET_PREV(head) = NULL;
+   	        munmap(ptr, GET_MMPSIZE(ptr));
+         	return;
+	    }
+     	    else if(GET_NEXT(ptr) == NULL) // ptr is at tail
+	    {
+		GET_NEXT(GET_PREV(ptr)) = NULL;
+ 	        munmap(ptr, GET_MMPSIZE(ptr));
+		return;
+	    }
+	    else // ptr is somewhere in middle of LL in this case
+	    {
+    	        GET_NEXT(GET_PREV(ptr)) = GET_NEXT(ptr);
+		GET_PREV(GET_NEXT(ptr)) = GET_PREV(ptr);
+		munmap(ptr, GET_MMPSIZE(ptr));
+		return;
+	    }
 	}
     }
+    // Can't munmap ptr
     return;
 }
 
 
 
 /* Push newly mmaped block or freed block to linked list and merge it if possible */
-static void push(void * ptr)
+static void push(ll * ptr)
 {
-    ptr -= SL;  // move ptr to proper position
-    void * temp = head;
-    while (temp != NULL)
+//    ptr -= SL;  // move ptr to proper position
+    ll * temp = head;
+    if (temp == NULL)
     {
-	// find proper position in free memory LL to insert given ptr into
-	if (ptr < temp) //insert ptr to the left of temp (which has to be head) and reassign head to ptr
-	{
-	    GET_NEXT(ptr) = temp;
-	    GET_PREV(ptr) = NULL;
-	    GET_PREV(temp) = ptr;
-	    head = ptr;
-	    break;
-	}
-	else // (ptr > temp)
-	{
-	    if (GET_NEXT(temp) == NULL) // if at tail, insert ptr at the end of free memory LL
+	head = ptr;
+	return;
+    }
+    else
+    {
+        while (temp != NULL)
+        {
+	    // find proper position in free memory LL to insert given ptr into
+	    if (ptr < temp) //insert ptr to the left of temp (which has to be head) and reassign head to ptr
 	    {
-		GET_NEXT(temp) = ptr;
-		GET_NEXT(ptr) = NULL;
-		GET_PREV(ptr) = temp;
-		break;
+	        GET_NEXT(ptr) = temp;
+	        GET_PREV(ptr) = NULL;
+	        GET_PREV(temp) = ptr;
+	        head = ptr;
+   	        return;
 	    }
-	    else // (temp->next != NULL)
-	    {
-	        if (ptr < GET_NEXT(temp)) // insert ptr inbetween temp and temp->next
+	    else // (ptr > temp)
+  	    {
+	        if (GET_NEXT(temp) == NULL) // if at tail, insert ptr at the end of free memory LL
 	        {
-		    GET_NEXT(ptr) = GET_NEXT(temp);
-		    GET_PREV(ptr) = temp;
 	            GET_NEXT(temp) = ptr;
-		    GET_PREV(GET_NEXT(ptr)) = ptr;
-		    break;
+		    GET_NEXT(ptr) = NULL;
+		    GET_PREV(ptr) = temp;
+		    return;
+	        }
+	        else // (temp->next != NULL)
+	        {
+	            if ((void *)ptr < GET_NEXT(temp)) // insert ptr inbetween temp and temp->next
+	            {
+		        GET_NEXT(ptr) = GET_NEXT(temp);
+		        GET_PREV(ptr) = temp;
+	                GET_NEXT(temp) = ptr;
+		        GET_PREV(GET_NEXT(ptr)) = ptr;
+		        return;
+	            }
 	        }
 	    }
-	}
 
-    // (ptr > temp->next), traverse further down the free memory LL
-    temp = GET_NEXT(temp);
+        // (ptr > temp->next), traverse further down the free memory LL
+        temp = GET_NEXT(temp);
+        }
     }
 }
 
 
 
 // After pushing to proper position, attempt to merge with surrounding nodes if possible
-static void merge(void * ptr)
+static void merge(ll * ptr)
 {
-    ptr = (char *) ptr; // cast ptr to a char ptr so we can increment by one byte at a time
+//    ptr = (char *) ptr; // cast ptr to a char ptr so we can increment by one byte at a time
     if (ptr == NULL) // ptr is NULL
     {
 	return;
     }
     else if (GET_PREV(ptr) == NULL && GET_NEXT(ptr) == NULL) // ptr is only value in LL
     {
+	tryMunmap(ptr);
 	return;
     }
     else if (GET_PREV(ptr) == NULL) // at head of LL, just check if we can merge with right node
     {
 	//check if one starts where the other ends and they are part of the same mmapping
-	if ((ptr + (GET_LENGTH(ptr)) + BYTE) == GET_NEXT(ptr) && GET_MMPSTART(ptr) == GET_MMPSTART(GET_NEXT(ptr)))
+	if (((void *)ptr + (GET_LENGTH(ptr)) + BYTE) == GET_NEXT(ptr) && GET_MMPSTART(ptr) == GET_MMPSTART(GET_NEXT(ptr)))
 	{
 	    //merge current node with next node
 	    GET_LENGTH(ptr) = (GET_LENGTH(ptr) + GET_LENGTH(GET_NEXT(ptr)));
@@ -336,7 +371,7 @@ static void merge(void * ptr)
     }
     else if (GET_NEXT(ptr) == NULL) // at tail of LL, just check if ptr can merge with left node
     {
-	if ((GET_PREV(ptr) + (GET_LENGTH(GET_PREV(ptr)) + BYTE)) == ptr && GET_MMPSTART(GET_PREV(ptr)) == GET_MMPSTART(ptr))
+	if ((GET_PREV(ptr) + (GET_LENGTH(GET_PREV(ptr)) + BYTE)) == (void *)ptr && GET_MMPSTART(GET_PREV(ptr)) == GET_MMPSTART(ptr))
 	{
 	    //merge left node with current node
 	    GET_LENGTH(GET_PREV(ptr)) = (GET_LENGTH(GET_PREV(ptr)) + GET_LENGTH(ptr));
@@ -350,7 +385,7 @@ static void merge(void * ptr)
     else // ptr is not at head or end of LL
     {
 	// check if we can merge with both left and right node
-	if ((GET_PREV(ptr) + GET_LENGTH(GET_PREV(ptr) + BYTE)) == ptr && GET_MMPSTART(GET_PREV(ptr)) == GET_MMPSTART(ptr) && (ptr + GET_LENGTH(ptr) + BYTE) == GET_NEXT(ptr) && GET_MMPSTART(ptr) == GET_MMPSTART(GET_NEXT(ptr)))
+	if ((GET_PREV(ptr) + GET_LENGTH(GET_PREV(ptr) + BYTE)) == (void *)ptr && GET_MMPSTART(GET_PREV(ptr)) == GET_MMPSTART(ptr) && (ptr + GET_LENGTH(ptr) + BYTE) == GET_NEXT(ptr) && GET_MMPSTART(ptr) == GET_MMPSTART(GET_NEXT(ptr)))
 	{
 	    GET_LENGTH(GET_PREV(ptr)) = (GET_LENGTH(GET_PREV(ptr)) + GET_LENGTH(ptr) + GET_LENGTH(GET_NEXT(ptr)));
 	    GET_NEXT(GET_PREV(ptr)) = GET_NEXT(GET_NEXT(ptr));
@@ -359,7 +394,7 @@ static void merge(void * ptr)
 	    return;
 	}
 	// check if we can merge with left node
-	else if ((GET_PREV(ptr) + GET_LENGTH(GET_PREV(ptr)) + BYTE) == &ptr && GET_MMPSTART(GET_PREV(ptr)) == GET_MMPSTART(ptr))
+	else if ((GET_PREV(ptr) + GET_LENGTH(GET_PREV(ptr)) + BYTE) == (void *)ptr && GET_MMPSTART(GET_PREV(ptr)) == GET_MMPSTART(ptr))
 	{
 	    //merge left node with current node
 	    GET_LENGTH(GET_PREV(ptr)) = (GET_LENGTH(GET_PREV(ptr)) + GET_LENGTH(ptr));
@@ -370,7 +405,7 @@ static void merge(void * ptr)
 
 	}
 	// check if we can merge with right node
-	else if((ptr + GET_LENGTH(ptr) + BYTE) == GET_NEXT(ptr) && GET_MMPSTART(ptr) == GET_MMPSTART(GET_NEXT(ptr)))
+	else if(((void *)ptr + GET_LENGTH(ptr) + BYTE) == GET_NEXT(ptr) && GET_MMPSTART(ptr) == GET_MMPSTART(GET_NEXT(ptr)))
 	{
 	    //merge current node with next node
 	    GET_LENGTH(ptr) = (GET_LENGTH(ptr) + GET_LENGTH(GET_NEXT(ptr)));
@@ -395,100 +430,114 @@ static void merge(void * ptr)
 //void *__malloc_impl(size_t size);
 // We need to return a ptr to the region starting where the head ends
 void *__malloc_impl(size_t size) {
-		// check if theres a space available in the free_memory LL and use from there first if possible
-		// if not, create a new space with mmap.
-    if (size < 1)
+    // check if theres a space available in the free_memory LL and use from there first if possible
+    // if not, create a new space with mmap.
+    void * temp1;
+//    ll * temp2;
+
+    if (size == (size_t) 0)
     {
-      return NULL;
+        return NULL;
     }
-    else if (size >9223372036854775807)
+    else if (size > 9223372036854775807)
     {
-      return NULL;
+        return NULL;
     }
     else
     {
-    // Search free memory LL for a space large enough to fit the size requested
-    void * temp = searchLL(size);
+        // Search free memory LL for a space large enough to fit the size requested
+        temp1 = searchLL(size);
 
         // If we cant find a large enough preexisting space in the free memory LL, mmap in a new space & add it to LL
-        if (temp == NULL)
+        if (temp1 == NULL)
         {
-            temp = mmap(NULL, size + SL, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS , -1, 0);
-            if (temp == NULL)
+            temp1 = mmap(NULL, size + SL, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS , -1, 0);
+            if (temp1 == NULL)
 	    {
 	        return NULL; // mmap failed to find space requested
 	    }
-	    GET_MMPSIZE(temp) = size + SL;
-	    GET_MMPSTART(temp) = temp;
-	    GET_LENGTH(temp) = size + SL;
-	    push(temp);
+//	    temp2 = (ll *) temp1;
+//	    temp1 = *(struct ll *)temp1;
+	    GET_MMPSIZE(temp1) = size + SL;
+	    GET_MMPSTART(temp1) = temp1;
+	    GET_LENGTH(temp1) = size + SL;
+
+	    push(temp1);
     	    // Search free memory LL for a space large enough to fit the size requested
-    	    void * temp = searchLL(size);
-            if (temp == NULL)
+    	    temp1 = searchLL(size);
+//	    temp2 = (ll *) temp1;
+            if (temp1 == NULL)
 	    {
 	        return NULL; // mmap failed to find space requested
 	    }
-	    temp += SL;
-	    return temp;
+//	    temp3 = (char *)temp2 + SL;
+//	    temp2 = (ll *)temp3;
+//	    temp2 += SL;
+	    temp1 += SL;
+	    return temp1;
         }
-	// We were able to find a large enough preexisting space in free memory LL
+        // We were able to find a large enough preexisting space in free memory LL
         else
-	{
-	    temp += SL;
-	    return temp;
+        {
+	    temp1 += SL;
+	    return temp1;
 	}
     }
 }
 
-void *__calloc_impl(size_t nmemb, size_t size) {
-  size_t aa = 0;
-  size_t * a = &aa;
-   __try_size_t_multiply(a,size,nmemb);
 
-  if (nmemb < 1 || size < 1 || *a >9223372036854775807 )
-  {
-  return NULL;
-  }
+void *__calloc_impl(size_t nmemb, size_t size)
+{
+    size_t aa = 0;
+    size_t * a = &aa;
+    __try_size_t_multiply(a,size,nmemb);
 
-  else{
-   void * temp = __malloc_impl(*a);
-   __memset(temp,0,*a);
-   temp += SL;
+    if (nmemb < 1 || size < 1 || *a >9223372036854775807 )
+    {
+        return NULL;
+    }
 
+    else
+    {
+        void * temp1 = __malloc_impl(*a);
+//	ll * temp2;
+//	temp2 = (ll *) temp1;
+        __memset(temp1,0,*a);
 
-  return temp;  
-  }
+        return temp1;
+    }
 }
+
 
 void *__realloc_impl(void *ptr, size_t size)
 {
     if (ptr)
     {
-        ptr -= sizeof(ll);
+        ptr -= SL;
         if (size == 0)
 	{
-            ptr += sizeof(ll);
+            ptr += SL;
             __free_impl(ptr);
    	    return NULL;
         }
-        else if (size < 0)
+        else if (size < (size_t)0)
   	{
    	    ptr += SL;
    	   __free_impl(ptr);
   	    return NULL;
   	}
- 	else if (size >9223372036854775807)
+ 	else if (size > (size_t)9223372036854775807)
         {
             ptr += SL;
             __free_impl(ptr);
             return NULL;
  	}
-        else if (size == GET_MMPSIZE(ptr)-sizeof(ll))
+        else if (size == GET_MMPSIZE(ptr) - SL)
         {
             ptr+=SL;
             return ptr;
         }
-        else if (size < GET_MMPSIZE(ptr)-sizeof(ll)) // realloc to smaller size
+        else if (size < GET_MMPSIZE(ptr) - SL) // realloc to smaller size
         {
             void * temp = __malloc_impl(size);
             ptr += SL;
@@ -499,7 +548,7 @@ void *__realloc_impl(void *ptr, size_t size)
         else // realloc to larger size
         {
             void * temp = __malloc_impl(size);
-   	    ptr += sizeof(ll);
+   	    ptr += SL;
             void * tempp = ptr - SL;
             __memcpy(temp,ptr,GET_MMPSIZE(tempp)-SL);
             __free_impl(ptr);
@@ -512,27 +561,36 @@ void *__realloc_impl(void *ptr, size_t size)
     }
 }
 
-void __free_impl(void *ptr)
+
+void __free_impl(void * ptr)
 {
 /* 1. Push into a linked list.
    2. sort linked list.
    3. merge blocks.
    4. if all blocks are returned free.
-*/
+32*/
     if (ptr == NULL)
     {
         return;
     }
-
+    else
+    {
 //if (ptr)
 //{
 //ptr -= sizeof(ll);
 //munmap(ptr,GET_MMPSIZE(ptr));
 //}
-
+    ptr -= SL;
+//    ll * temp;
+//    temp = (ll *)&ptr;
     push(ptr);
     merge(ptr);
     return;
-
+    }
 
 }
+
+
+
+
+
